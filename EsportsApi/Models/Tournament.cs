f890bf4a6a5ef7d0abd4505ec08f4613dd5fcab5
Mateo Alba Kondo <a2024110819@estudiantes.upsa.edu.bo>
2025-11-16 15:49:21 -0400

    anadir rol de organizador a tournament.cs

--- EsportsApi/Models/Tournament.cs
+++ EsportsApi/Models/Tournament.cs
@@ -1,4 +1,6 @@
 // En Models/Tournament.cs
+using System.ComponentModel.DataAnnotations.Schema; // (Añadimos esto)
+
 namespace EsportsApi.Models
 {
     public class Tournament
@@ -7,5 +9,13 @@ namespace EsportsApi.Models
         public string Name { get; set; }
         public string Game { get; set; }
         public DateTime StartDate { get; set; }
+
+        // --- LÍNEAS NUEVAS AÑADIDAS ---
+        // Esto le dice a la BD quién es el "dueño"
+        public int OrganizadorId { get; set; } 
+        
+        [ForeignKey("OrganizadorId")]
+        public virtual User Organizador { get; set; } 
+        // -------------------------------
     }
 }
